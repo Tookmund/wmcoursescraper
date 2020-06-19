4659a893c15383773c5fc1900f26170fd9479f02
Jacob Adams <jacob@tookmund.com>
2020-06-19 16:18:34 -0400

    runscraper: Don't delete the index page and make sure db files are accessible

--- runscraper.sh
+++ runscraper.sh
@@ -4,7 +4,8 @@ set -e
 cd /home/scraper
 wmcoursescraper/scraper.py
 cd /var/www/html
-find . -mtime +29 -exec rm {} \;
+find . -name '*.db' -mtime +29 -exec rm {} \;
 TODAY=$(date -I)
 mv /home/scraper/courses.db "$TODAY.db"
+chown scraper:www-data "$TODAY.db"
 ln -sf "$TODAY.db" courses.db
