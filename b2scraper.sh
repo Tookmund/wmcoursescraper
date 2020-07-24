e55e16a782d5f5c785322e7e233b5338de58d352
Jacob Adams <jacob@tookmund.com>
2020-07-24 19:32:05 -0400

    sqlite2csv: Split out CSV conversion code

--- b2scraper.sh
+++ b2scraper.sh
@@ -7,15 +7,5 @@ TODAY=$(date -I)
 mv courses.db "$TODAY.db"
 find . -name '*.db' -mtime +29 -exec rm {} \;
 ln -sf "$TODAY.db" courses.db
-for t in $(sqlite3 "$TODAY.db" "SELECT name FROM sqlite_master where type='table' and name != 'subjects' and name != 'semesterdates';")
-do
-
-sqlite3 "$TODAY.db" << EOF
-.headers on
-.mode csv
-.output $t.csv
-SELECT * FROM $t;
-EOF
-
-done
+../wmcoursescraper/sqlite2csv.sh "$TODAY.db"
 backblaze-b2 sync --delete . b2://wmcoursescraper
---
+++ sqlite2csv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+for t in $(sqlite3 "$1" "SELECT name FROM sqlite_master where type='table' and name != 'subjects' and name != 'semesterdates';")
+do
+
+sqlite3 "$1" << EOF
+.headers on
+.mode csv
+.output $t.csv
+SELECT * FROM $t;
+EOF
+
+done
