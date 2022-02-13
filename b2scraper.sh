f2a22daa2cb016458bf3efaa7a3b8ba282549ea8
Jacob Adams <jacob@tookmund.com>
2022-02-12 19:00:46 -0500

    b2scraper: Keep the last 90 days of courses

--- b2scraper.sh
+++ b2scraper.sh
@@ -5,7 +5,7 @@ cd "$HOME/courses.db.d"
 time ../wmcoursescraper/scraper.py
 TODAY=$(date -I)
 mv courses.db "$TODAY.db"
-find . -name '*.db' -mtime +29 -exec rm {} \;
+find . -name '*.db' -mtime +90 -exec rm {} \;
 ln -sf "$TODAY.db" courses.db
 ../wmcoursescraper/sqlite2csv.sh "$TODAY.db"
 time backblaze-b2 sync --noProgress --delete . b2://wmcoursescraper
