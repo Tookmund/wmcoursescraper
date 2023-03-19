78eafb0242caf5a05072060d4d5b08985e9047ca
Jacob Adams <jacob@tookmund.com>
2023-03-19 14:52:54 -0400

    b2scraper: Run the scraper via venv python

--- b2scraper.sh
+++ b2scraper.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd "$HOME/courses.db.d"
-time ../wmcoursescraper/scraper.py
+time ../wmcoursescraper/venv/bin/python3 ../wmcoursescraper/scraper.py
 TODAY=$(date -I)
 mv courses.db "$TODAY.db"
 find . -name '*.db' -mtime +90 -exec rm {} \;
